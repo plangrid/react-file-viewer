77180884ea61827f2b49eb80077cb20e02896808
Aaron Norby <aaron.norby@plangrid.com>
2017-05-17 13:37:59 -0700

    Demo app update

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FileViewer from './components/file-viewer';
 import sampleHouse from '../example_files/SampleHouse.wexbim';
+import solarImage from '../example_files/02-USVI-Solar.jpg';
 
 // filePath='https://data.cityofnewyork.us/api/views/3h6b-pt5u/rows.csv'
 ReactDOM.render(
