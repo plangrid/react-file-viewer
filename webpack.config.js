1b89d32dd06ba5b6e3c406bb1b4e6c3bed5b4c68
Alexei Schiopu <alexei@plangrid.com>
2017-05-16 19:39:14 -0700

    0.1.7

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var BUILD_DIR = path.resolve(__dirname, './dist');
 var APP_DIR = path.resolve(__dirname, './src');
 
 var config = {
-  entry: APP_DIR + '/index.js',
+  entry: APP_DIR + '/components/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'index.js'
