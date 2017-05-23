1fead1d0bfb5d9e885a2d42ff93322df3744294d
Alexei Schiopu <alexei@plangrid.com>
2017-05-23 12:57:34 -0700

    0.3.39

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,24 @@ const config = {
     library: 'FileViewer',
     libraryTarget: 'umd',
   },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+  ],
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules'],
     extensions: ['.js', '.jsx', '.json'],
