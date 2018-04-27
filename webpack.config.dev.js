1591510a2bc84a52aab987645f9f92baf3e006a5
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2018-04-26 17:02:17 -0700

    Pdf viewer (#66)
    * add pdf example
    
    * properly import PDFJS

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -73,7 +73,7 @@ module.exports = {
         enforce: 'pre',
       },
       {
-        test: [/\.wexbim$/, /\.jpg$/, /\.docx$/, /\.csv$/, /\.mp4$/, /\.xlsx$/, /\.doc$/, /\.avi$/, /\.webm$/, /\.mov$/, /\.mp3$/, /\.rtf$/],
+        test: [/\.wexbim$/, /\.jpg$/, /\.docx$/, /\.csv$/, /\.mp4$/, /\.xlsx$/, /\.doc$/, /\.avi$/, /\.webm$/, /\.mov$/, /\.mp3$/, /\.rtf$/, /\.pdf$/],
         loader: 'file-loader',
       },
       {
