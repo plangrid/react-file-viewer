b71e25c146cd5515a9c221d06a58ec37f11e24d5
Bryan Alexander Rivera <2250179+bryaan@users.noreply.github.com>
2019-07-28 05:03:23 -0400

    Update file-viewer.jsx
    Pass through style props.

--- src/components/file-viewer.jsx
+++ src/components/file-viewer.jsx
@@ -73,8 +73,8 @@ class FileViewer extends Component {
   render() {
     const Driver = this.getDriver(this.props);
     return (
-      <div className="pg-viewer-wrapper">
-        <div className="pg-viewer" id="pg-viewer">
+      <div style={wrapperStyle} className="pg-viewer-wrapper">
+        <div style={style} className="pg-viewer" id="pg-viewer">
           <Driver {...this.props} width={this.state.width} height={this.state.height} />
         </div>
       </div>
@@ -83,6 +83,8 @@ class FileViewer extends Component {
 }
 
 FileViewer.propTypes = {
+  wrapperStyle: PropTypes.string,
+  style: PropTypes.string,
   fileType: PropTypes.string.isRequired,
   filePath: PropTypes.string.isRequired,
   onError: PropTypes.func,
